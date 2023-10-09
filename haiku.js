6622057489e99ecad26d3f864b8eca8eed988b56
Arturo <38075@ma-web.nl>
2023-10-09 09:32:42 +0200

    Arturo's haiku toegevoegd

--- haiku.js
+++ haiku.js
@@ -2,6 +2,13 @@
 
 #plak hier de haiku functions onder elkaar
 
+//Arturo's Haiku
+function Haiku1(){
+    console.log("haiku by:","Arturo");
+    console.log("In the cicada's cry");
+    console.log("No sign can foretell");
+    console.log("How soon it must die.");
+}
 
 
 
